bdf4ccc219b7ef5833fe44840ad13529186603b3
endiawilliams <endiawilliams1@gmail.com>
2021-01-11 11:02:22 -0600

    Updates corsOptions to localhost

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(express.json())
 // middleware - cors
 const corsOptions = {
   // from which URLs do we want to accept requests
-  origin: ['http://34.122.25.56:3000', 'http://localhost:3000'],
+  origin: 'http://localhost:3000',
   credentials: true, // allow the session cookie to be sent to and from the client
   optionsSuccessStatus: 204
 }
